c083f9c7f11ae45ecdeb3337b41231334dbb26ac
vikas paul <yogivikaspaul@gmail.com>
2020-01-04 13:25:47 +0530

    first Commit

---
+++ src/test/java/com/vikas/test/mavenaws/MavenawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.test.mavenaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
